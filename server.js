f32d27070fdbdd15d376ddcdd2f77962ef1a2177
Evan Busse <evanbusse@gmail.com>
2017-04-23 00:22:45 -0600

    heroku logs

--- server.js
+++ server.js
@@ -27,8 +27,7 @@ app.listen(PORT, function () {
 });
 
 app.get('/color/:text', (req, res) => {
-  var text = decodeURI(req.params.text).replace('%20', ' ');
-
+  var text = req.params.text.replace('%20', ' ');
 
   console.log(text);
 
